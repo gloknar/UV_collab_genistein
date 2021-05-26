09944b5f3352943c6dc444001d54074a25fe3a6f
gloknar <58296829+gloknar@users.noreply.github.com>
2021-05-26 14:47:09 +0200

    Código refactorizado

--- Estudios previos/TFM Elisa Sanchez 2019/prueba ajuste p-valor.R
+++ Estudios previos/TFM Elisa Sanchez 2019/prueba ajuste p-valor.R
@@ -1,6 +1,7 @@
 list.files()
 
-base_datos_miRNA <- readxl::read_excel(path =  "todos mirna 6632 liberados por las mononuclerares.xls", sheet = 2)
+base_datos_miRNA <- readxl::read_excel(path =  "todos mirna 6632 liberados por las mononuclerares.xls", 
+                                       sheet = 2)
 
 str(base_datos_miRNA)
 dim(base_datos_miRNA)
@@ -20,14 +21,16 @@ min(p_valores_ajustados)
 
 list.files()
 
-base_datos_miRNA <- readxl::read_excel(path =  "todos mirna 6632 liberados por las mononuclerares.xls", sheet = 1)
+base_datos_miRNA <- readxl::read_excel(path =  "todos mirna 6632 liberados por las mononuclerares.xls", 
+                                       sheet = 1)
 
 str(base_datos_miRNA)
 dim(base_datos_miRNA)[1]
 
 head(base_datos_miRNA$"p-value(GENISTEIN vs. CONTROL)")
 
-p_valores_ajustados <- p.adjust(base_datos_miRNA$"p-value(GENISTEIN vs. CONTROL)", method = "fdr", n = 6631)
+p_valores_ajustados <- p.adjust(base_datos_miRNA$"p-value(GENISTEIN vs. CONTROL)", 
+                                method = "fdr", n = 6631)
 min(p_valores_ajustados)
 
 
