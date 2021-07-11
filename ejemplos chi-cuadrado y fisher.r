29d22f372da5185189c485b1ad1792004a5dcb86
gloknar <mipeeeerro@gmail.com>
2021-07-11 10:56:46 +0200

    Update ejemplos chi-cuadrado y fisher.r

--- ejemplos chi-cuadrado y fisher.r
+++ ejemplos chi-cuadrado y fisher.r
@@ -22,3 +22,9 @@ test2 <- fisher.test(dat)
 test2
 
 test2$p.value
+
+
+# rejecting the null hypothesis for the Fisher’s exact test of independence
+# means that there is a significant relationship between the two categorical
+# variables (smoking habits and being an athlete or not). Therefore, knowing the
+# value of one variable helps to predict the value of the other variable.
