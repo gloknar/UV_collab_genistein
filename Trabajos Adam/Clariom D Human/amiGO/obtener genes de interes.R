fceabc0b60e175ccfe895f799b109395e8139efa
ACM <mipeeeerro@gmail.com>
2021-11-13 21:33:43 +0100

    .

---
+++ Trabajos Adam/Clariom D Human/amiGO/obtener genes de interes.R
@@ -0,0 +1,19 @@
+# Leemos tabla de resultados del análisis en limma
+DEG_limma <- readRDS("tabla_resultados_total.rds")
+View(DEG_limma)
+
+# Leemos la lista de genes con actividad antioxidante de acuerdo a amiGO2 (la 2a
+# columna es el nombre oficial, y la 3a son los sinónimos)
+df_genes_antioxidantes <- read.table("Genes homo sapiens anotados actividad anbtioxidante.txt", fill = T)
+
+# Ídem para genes que regulan actividad antioxidante
+df_genes_regular_antioxidantes <- read.table("reguladores actividad antioxidante.txt", fill = T)
+df_genes_regular_antioxidantes <- df_genes_regular_antioxidantes[-42,]
+
+View(DEG_limma)
+DEG_limma[df_genes_antioxidantes$V2,]
+
+
+genes_interes <- which(DEG_limma$P.Value <= 0.05 & abs(df_DEG$logFC) >= 1)
+DEG_limma <- DEG_limma[genes_diferencialmente_expresados,]
+
